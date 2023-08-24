ed14ad1e49f016148835f02733feedf423bc8142
Gunjesh <gunjesh.basnet550208@gmail.com>
2023-08-24 23:44:24 +0545

    build update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import Location from "./components/Location";
 import places from "./data/content.json";
 
-import "./styles/main.css";
-
 function App() {
   return (
     <div className="h-screen w-screen">
