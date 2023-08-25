013622ed71d42673bb4338e28e013d721c25e57a
Gunjesh <gunjesh.basnet550208@gmail.com>
2023-08-25 13:29:02 +0545

    update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <main className="h-full max-w-[90rem] mx-auto px-5 flex flex-col">
         <h1 className="font-bold text-5xl mb-[49px] mt-[71px]">CITIES TO VISIT IN EUROPE</h1>
         <section className="flex flex-wrap pb-7">
-          {places.map((place, index) => (
-            <Location key={index} name={place.name} country={place.country} thumbnail={place.thumbnail} description={place.description} />
+          {places.map((place, idx) => (
+            <Location key={idx} name={place.name} country={place.country} thumbnail={place.thumbnail} description={place.description} />
           ))}
         </section>
       </main>
